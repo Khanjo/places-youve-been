493054fcafbabe10de78439af67d8ba44399b851
Joshua Khan <Khanjo@gmail.com>
2023-06-12 15:58:30 -0700

    create UI logic
    co-Authored-by: Jessica Perry <l33thedgieperry@gmail.com>
    co-Authored-by: Jase Grable <jase.grable@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,6 @@
+//Business Logic
 function Places() {
     this.place = {};
-}
-
-Places.prototype.addPlace = function (placeBeen) {
-    this.place[placeBeen.location] = place;
 }
 
 function Place(location, date, hotel, note) {
@@ -13,6 +10,22 @@ function Place(location, date, hotel, note) {
     this.note = note;
 }
 
+Places.prototype.addPlace = function (placeBeen) {
+    this.place[placeBeen.location] = placeBeen;
+}
 
+//User Interface Logic
+window.addEventListener("load", function () {
+    let form = document.querySelector("form");
+    const placesYouBeen = new Places();
+    form.addEventListener("submit", function (event) {
+        event.preventDefault();
+        let loc = document.getElementById("loc").value;
+        let time = document.getElementById("time").value;
+        let hotel = document.getElementById("hotel").value;
+        let description = document.getElementById("description").value;
+        let newPlace = Place(loc, time, hotel, description);
+        console.log(newPlace);
+    })
 
-let newPlace = Place("Aruba", "10/12/20", "Hyatt", "was beautiful");
+})
